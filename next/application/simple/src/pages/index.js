eee211bc768c49a664adbfe6b09edd652b43cc2b
Bruno Franki <bruno.franki@gmail.com>
2020-05-28 20:49:33 +0200

    fix warning application/simple

--- next/application/simple/src/pages/index.js
+++ next/application/simple/src/pages/index.js
@@ -4,19 +4,19 @@ import Link from "next/link";
 export default () => {
   return (
     <div>
-      <p>This is very basic example. Check the files
+      <div>This is very basic example. Check the files
         <ul>
           <li><b>package.json</b>: to see the dependencies</li>
           <li><b>_app.js</b>: to find how the application is bootstrapped</li>
         </ul>
-      </p>
-      <p>
+      </div>
+      <div>
         This simple example bootstraps:
         <ul>
           <li><b>A router</b>: Go to <Link href="/users">Users page</Link></li>
           <li><b>A Redux store</b></li>
         </ul>
-      </p>
+      </div>
     </div>
   );
 };
