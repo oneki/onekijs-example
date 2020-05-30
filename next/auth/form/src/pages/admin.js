60140cf5cd647b6e30904a6bcde2031d9bf8c52d
Bruno Franki <bruno.franki@gmail.com>
2020-05-30 21:05:28 +0200

    rename UserPage to AdminPage

--- next/auth/form/src/pages/admin.js
+++ next/auth/form/src/pages/admin.js
@@ -2,10 +2,10 @@ import { withLayout, secure } from 'onekijs';
 import React from 'react';
 import MainLayout from '../layout/mainLayout/MainLayout';
 
-const UsersPage = () => {
+const AdminPage = () => {
   return (
     <div>This is the (protected) admin page</div>
   );
 }
 
-export default secure(withLayout(UsersPage, MainLayout));
+export default secure(withLayout(AdminPage, MainLayout));
